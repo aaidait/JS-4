95ef16a977a71449f4b544a58a327b33f8562673
AIDAI <turdakunovaidai@gmail.com>
2023-10-26 16:20:41 +0600

    first

---
+++ main.js
@@ -0,0 +1,39 @@
+// Task-1
+let a = 6 
+let sum = 0; 
+if (a >= 0) {
+    for (let i = 1; i<=a; i++ ) {
+        sum += i
+    }
+    console.log (sum);
+
+}
+
+
+
+// //   Task-2: Треугольник в цикле
+
+// let str= "#";
+
+// while (str.length<=7 ){
+//     console.log (str + "\n");
+
+//     str += "#";
+// }
+
+
+// // Task-3: FizzBuzz
+
+// for (let i = 1; i <= 100; i ++) {
+
+//     if (i % 3 === 0 && i % 5 === 0){
+//         console.log ("FIZZBUZZ");
+//     } else if (i % 3 === 0) {
+//         console.log ("FIZZ");
+//     } else if (i % 5 === 0){
+//         console.log("BUZZ");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
