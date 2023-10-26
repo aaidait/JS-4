04ce9116ef6c00b5c810a7b86938b28e18e63b9c
AIDAI <turdakunovaidai@gmail.com>
2023-10-26 16:24:01 +0600

    third

--- main.js
+++ main.js
@@ -24,16 +24,16 @@ while (str.length<=7 ){
 
 // // Task-3: FizzBuzz
 
-// for (let i = 1; i <= 100; i ++) {
+for (let i = 1; i <= 100; i ++) {
 
-//     if (i % 3 === 0 && i % 5 === 0){
-//         console.log ("FIZZBUZZ");
-//     } else if (i % 3 === 0) {
-//         console.log ("FIZZ");
-//     } else if (i % 5 === 0){
-//         console.log("BUZZ");
-//     } else {
-//         console.log(i);
-//     }
-// }
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log ("FIZZBUZZ");
+    } else if (i % 3 === 0) {
+        console.log ("FIZZ");
+    } else if (i % 5 === 0){
+        console.log("BUZZ");
+    } else {
+        console.log(i);
+    }
+}
 
